54ad6fe92c2927b07056d99a9afc1fecc24e2935
BeserWOB <beser574@gmail.com>
2023-07-03 17:05:52 +0200

    Some more changes.

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ window.addEventListener('scroll', function() {
       header.classList.remove('navbar-scroll');
     }
   });
+
+
+
+  window.addEventListener('scroll', function() {
+    let carouselSection = document.getElementById('images-section');
+    let header = document.querySelector('.header');
+    let navbarBottom = document.querySelector('.header').getBoundingClientRect().bottom;
+  
+    let carouselSectionTop = carouselSection.getBoundingClientRect().top;
+    let carouselSectionBottom = carouselSection.getBoundingClientRect().bottom;
+  
+    if (carouselSectionTop < navbarBottom && carouselSectionBottom > navbarBottom) {
+      header.classList.add('transparent-header');
+    } else {
+      header.classList.remove('transparent-header');
+    }
+  });
+  
