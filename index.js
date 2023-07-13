9e6599091989cf7c41731f858183faa17d43b5e5
BeserWOB <beser574@gmail.com>
2023-07-13 16:04:04 +0200

    Almost done.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ window.addEventListener("scroll", function () {
 });
 
 /* Onscroll Header is transparent when it comes to images section */
-/* 
+
 window.addEventListener("scroll", function () {
   let carouselSection = document.getElementById("images");
   let header = document.querySelector(".header");
@@ -35,7 +35,7 @@ window.addEventListener("scroll", function () {
     header.classList.remove("transparent-header");
   }
 });
- */
+
 
 /* Onscroll animation of the SVG Icons for breed section */
 window.addEventListener("scroll", () => {
